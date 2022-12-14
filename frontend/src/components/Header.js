a09c02af80b694c11f5c3e12d5042e23c54fa363
Matheus Zufi Leite <matheuszufi@gmail.com>
2022-12-14 15:46:04 -0300

    Update

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+
+const Header = () => {
+  return (
+    <header>
+        <Navbar bg="dark" variant='dark' expand="lg" collapseOnSelect>
+            <Container>
+                <Navbar.Brand href="/">ProShop</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className='ml-auto'>
+                        <Nav.Link href="/cart">Cart <i className='fas fa-shopping-cart'></i></Nav.Link>
+                        <Nav.Link href="/login">Sign In <i className='fas fa-user'></i></Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    </header>
+  )
+}
+
+export default Header
